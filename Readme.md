211e6dd170f8db1b1e1b443b906303b8ee157bba
muhammadabubakar <abubakarasif26@outlook.com>
2023-06-01 18:58:30 +0200

    Changes in EC_point_generator method and addition of weierstrass equation B_generator

--- Readme.md
+++ Readme.md
@@ -1,2 +1,8 @@
 <h1>Lenstra's Elliptic curve.</h1>
 
+<p> Implementation of the Factorization of composite number using the Elliptic curves. 
+<br>
+Method proposed by lenstra and summarized by Daniel Parker
+</p>
+
+
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import math
 import random
 import numpy as np
-
-
+import sympy as sm
+from gmpy2 import f_mod
 def hasse_weil_bound(n: int) -> int:
     # Estimate the number of points on the elliptic curve
-    left_bound = n + 1 + math.floor(math.sqrt(n))
-    right_bound = n + 1 - math.floor(math.sqrt(n))
-    return left_bound, right_bound
+    if sm.isprime(n):
 
+        right_bound = n + 1 + math.floor(math.sqrt(n))
+        left_bound = n + 1 - math.floor(math.sqrt(n))
+        return [left_bound, right_bound]
+    return "Give prime number"
 
-def EC_singulaity_check(A: int, B: int, n: int) -> bool:
+def weierstrass_B(a,x,y,n) -> int:
+    b=((y*y)-(x*x*x)-(a*x))
+    b=(b%n)
+    return b
+
+def EC_singulaity_check(A, B, n) -> bool:
     # check condition whether the curve is singular or not if singular then it is discarded
-    if (math.gcd((4(A * A * A) + 27(B * B)), n)) != 0:
+    singular = (4*(A * A * A) + 27*(B * B))
+    if(math.gcd(singular,n)) != 0:
         return True
     else:
         False
@@ -27,15 +35,17 @@ def point_on_EC(a, b, x, y, n) -> bool:
     return False
 
 
-def Ec_Point_generator(a, b, n, l_bound, r_bound) -> list:
+def Ec_Point_generator(a, n, l_bound, r_bound) -> list:
     range = np.arange(2, r_bound)
     x_range = range
     y_range = range
     valid_points_on_EC = []
     for i in x_range:
         for j in y_range:
-            if point_on_EC(a, b, i, j, n):
-                valid_points_on_EC.append([i, j])
+            b = weierstrass_B(a, i, j, n)
+            if(EC_singulaity_check(a,b,n)):
+                if point_on_EC(a, b, i, j, n):
+                    valid_points_on_EC.append([i, j])
     return valid_points_on_EC
 
 
@@ -45,11 +55,24 @@ def k_generator() -> int:
     return k_lcm
 
 
-def lenstras_factorization(k, prime_num, point) -> int:
-    k*point
-    return 0
+def lenstras_factorization(k, prime_num, points) -> int:
+    factors_list=[]
+    for point in points:
+        X_p, Y_p = point
+        F_1 = math.gcd(k*X_p,prime_num)
+        F_2 = math.gcd(k*Y_p,prime_num)
+        factors_list.append([F_1,F_2])
+    return factors_list
 
 
 if __name__ == '__main__':
-    print(np.arange(2, 9))
-    print(math.lcm(5, 2912312083))
+
+    composite_integer = 5
+    A = 8
+    bound = hasse_weil_bound(composite_integer)
+    l_bound, r_bound = bound
+    points_list = Ec_Point_generator(A,composite_integer,l_bound,r_bound)
+    k = k_generator()
+    factors= lenstras_factorization(k,composite_integer,points_list)
+    print(((points_list)))
+    print(factors)

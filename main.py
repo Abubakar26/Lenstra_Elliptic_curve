336e79563cf2e807b8226a21e5c3863e901c28e7
muhammadabubakar <abubakarasif26@outlook.com>
2023-06-01 22:32:01 +0200

    Changes in point generator

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ from gmpy2 import f_mod
 def hasse_weil_bound(n: int) -> int:
     # Estimate the number of points on the elliptic curve
     if sm.isprime(n):
-
-        right_bound = n + 1 + math.floor(math.sqrt(n))
-        left_bound = n + 1 - math.floor(math.sqrt(n))
+        right_bound = n + 1 + math.floor(2*(math.sqrt(n)))
+        left_bound = n + 1 - math.floor(2*(math.sqrt(n)))
         return [left_bound, right_bound]
     return "Give prime number"
 
 def weierstrass_B(a,x,y,n) -> int:
-    b=((y*y)-(x*x*x)-(a*x))
-    b=(b%n)
+    b = ((y*y)-(x*x*x)-(a*x))
+    b = (b % n)
     return b
 
 def EC_singulaity_check(A, B, n) -> bool:
     # check condition whether the curve is singular or not if singular then it is discarded
     singular = (4*(A * A * A) + 27*(B * B))
-    if(math.gcd(singular,n)) != 0:
-        return True
+    calc_gcd = math.gcd(singular, n)
+    if(calc_gcd != 0):
+         if(calc_gcd>1 and calc_gcd<n):
+            return True
     else:
-        False
+      return False
 
 
 def point_on_EC(a, b, x, y, n) -> bool:
@@ -35,18 +36,19 @@ def point_on_EC(a, b, x, y, n) -> bool:
     return False
 
 
-def Ec_Point_generator(a, n, l_bound, r_bound) -> list:
-    range = np.arange(2, r_bound)
+def Ec_Point_generator(a, n, r_bound) -> list:
+    range = np.arange(1, r_bound)
     x_range = range
     y_range = range
-    valid_points_on_EC = []
+    points_on_EC = []
     for i in x_range:
         for j in y_range:
             b = weierstrass_B(a, i, j, n)
-            if(EC_singulaity_check(a,b,n)):
-                if point_on_EC(a, b, i, j, n):
-                    valid_points_on_EC.append([i, j])
-    return valid_points_on_EC
+            if point_on_EC(a, b, i, j, n):
+               points_on_EC.append([i, j])
+        if (EC_singulaity_check(a, b, n)):
+            return True
+    return points_on_EC
 
 
 def k_generator() -> int:
@@ -67,12 +69,13 @@ def lenstras_factorization(k, prime_num, points) -> int:
 
 if __name__ == '__main__':
 
-    composite_integer = 5
-    A = 8
+    composite_integer =41
+    A = 0
     bound = hasse_weil_bound(composite_integer)
     l_bound, r_bound = bound
-    points_list = Ec_Point_generator(A,composite_integer,l_bound,r_bound)
+    points_list = Ec_Point_generator(A,composite_integer,r_bound)
     k = k_generator()
     factors= lenstras_factorization(k,composite_integer,points_list)
-    print(((points_list)))
+    (print(l_bound,'',r_bound))
+    print(len((points_list)))
     print(factors)

5383080d9831348e6e3c6e7dad4e3fde1f000af0
muhammadabubakar <abubakarasif26@outlook.com>
2023-06-02 11:54:02 +0200

    Final version

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import random
 import numpy as np
 import sympy as sm
 from gmpy2 import f_mod
+import timeit
+
+
 def hasse_weil_bound(n: int) -> int:
     # Estimate the number of points on the elliptic curve
-    if sm.isprime(n):
-        right_bound = n + 1 + math.floor(2*(math.sqrt(n)))
-        left_bound = n + 1 - math.floor(2*(math.sqrt(n)))
-        return [left_bound, right_bound]
-    return "Give prime number"
+    right_bound = n + 1 + math.floor(2*(math.sqrt(n)))
+    left_bound = n + 1 - math.floor(2*(math.sqrt(n)))
+    return [left_bound, right_bound]
+    # return "Give prime number"
+
 
 def weierstrass_B(a,x,y,n) -> int:
     b = ((y*y)-(x*x*x)-(a*x))
@@ -21,8 +24,7 @@ def EC_singulaity_check(A, B, n) -> bool:
     singular = (4*(A * A * A) + 27*(B * B))
     calc_gcd = math.gcd(singular, n)
     if(calc_gcd != 0):
-         if(calc_gcd>1 and calc_gcd<n):
-            return True
+          return True
     else:
       return False
 
@@ -45,9 +47,9 @@ def Ec_Point_generator(a, n, r_bound) -> list:
         for j in y_range:
             b = weierstrass_B(a, i, j, n)
             if point_on_EC(a, b, i, j, n):
-               points_on_EC.append([i, j])
-        if (EC_singulaity_check(a, b, n)):
-            return True
+                if (EC_singulaity_check(a, b, n)):
+                    points_on_EC.append([i, j])
+                continue
     return points_on_EC
 
 
@@ -57,25 +59,30 @@ def k_generator() -> int:
     return k_lcm
 
 
-def lenstras_factorization(k, prime_num, points) -> int:
+def lenstras_factorization(k, n, points) -> int:
     factors_list=[]
     for point in points:
         X_p, Y_p = point
-        F_1 = math.gcd(k*X_p,prime_num)
-        F_2 = math.gcd(k*Y_p,prime_num)
+        F_1 = math.gcd(k*X_p,n)
+        F_2 = math.gcd(k*Y_p,n)
         factors_list.append([F_1,F_2])
     return factors_list
 
 
 if __name__ == '__main__':
 
-    composite_integer =41
-    A = 0
+    composite_integer =9000
+    A =random.randint(100,composite_integer)
+    print(A)
+    start = timeit.default_timer()
     bound = hasse_weil_bound(composite_integer)
     l_bound, r_bound = bound
     points_list = Ec_Point_generator(A,composite_integer,r_bound)
     k = k_generator()
-    factors= lenstras_factorization(k,composite_integer,points_list)
-    (print(l_bound,'',r_bound))
-    print(len((points_list)))
+    factors= lenstras_factorization(k, composite_integer, points_list)
+    stop = timeit.default_timer()
+    (print(l_bound, '<= E(F_P)<=', r_bound))
+    print((points_list))
     print(factors)
+    print('time elapsed', stop-start)
+

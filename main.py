cd0afd574d3fca599c172e6daa37c434b9e85490
muhammadabubakar <abubakarasif26@outlook.com>
2023-06-09 11:38:52 +0200

    Added the binary operation methods which can be performed on EC using (G, ., +) both methods are not completed yet. Point at infinity not defined yet properly

--- main.py
+++ main.py
@@ -60,35 +60,93 @@ def Ec_Point_generator(a, n, l_bound, r_bound) -> list:
     return points_on_EC
 
 
+
+
+
 def k_generator() -> int:
     k = random.randint(45321, 893483274)
     k_lcm = math.lcm(2, k)
     return k_lcm
 
 
-def lenstras_factorization(k, n, points) -> int:
-    factors_list=[]
+def lambda_calculate(P,Q)->int:
+    x_p,y_p=P
+    x_q, y_q = Q
+    lambdaa=((y_p - y_q)/(x_p-x_q))
+    return lambdaa
+
+
+def divisor_using_addition(n,points):
+    infinity_point=(0,1)
+    P= points[0]
+    Q= points[1]
+    x_p,y_p=P
+    x_q,y_q=Q
+
+    if (P and Q !=infinity_point):
+        d=math.gcd((x_p-x_q),n)
+        if(1<d<n):
+            print("We find the divisor")
+            return d
+        elif(d==1):
+         print("Euclidean Algorithm give Multiplicative inverse hence we found the third point")
+
+         lambdaa=lambda_calculate(P, Q)
+         x_R=((lambdaa*lambdaa)-x_p-x_q)
+         y_R=(lambdaa*(x_p-x_R)-y_p)
+         print(x_R)
+         print(y_R)
+         R=(x_R,y_R)
+         return(R)
+        elif(d==n):
+          return('X_p == X_q')
+
+def divisor_using_multiplication(k,n,points):
+    k = bin(k)
+    k = (k[2:])
+    k = k[::-1]
+    binary_points = []
+    P = []
+    for num, ones in enumerate(k):
+        if (int(ones) == 1):
+            binary_points.append(num)
+    print("binary", binary_points)
+
     for point in points:
-        X_p, Y_p = point
-        F_1 = math.gcd(k*X_p,n)
-        F_2 = math.gcd(k*Y_p,n)
-        factors_list.append([F_1,F_2])
-    return factors_list
+        print(point)
+        x, y = point
+        for binary in binary_points:
+            X_p = ((2 ** binary) * x)
+            Y_p = ((2 ** binary) * y)
+            Kp=(X_p,Y_p)
+            divisor_using_addition(n, Kp)
+    return 0
+
+
+# def lenstras_factorization(k, n, points) -> int:
+#     factors_list=[]
+#     for point in points:
+#         X_p, Y_p = point
+#         F_1 = math.gcd(k*X_p,n)
+#         F_2 = math.gcd(k*Y_p,n)
+#         factors_list.append([F_1,F_2])
+#     return factors_list
 
 
 if __name__ == '__main__':
 
-    composite_integer = 73
-    condition_check=num_check(composite_integer)
+    prime_num = 13
+    condition_check=num_check(prime_num)
     if(condition_check):
         A = random.randint(1000, 10000)
         print(A)
         start = timeit.default_timer()
-        bound = hasse_weil_bound(composite_integer)
+        bound = hasse_weil_bound(prime_num)
         l_bound, r_bound = bound
-        points_list = Ec_Point_generator(A,composite_integer,l_bound,r_bound)
+        points_list = Ec_Point_generator(A,prime_num,l_bound,r_bound)
         k = k_generator()
-        factors= lenstras_factorization(k, composite_integer, points_list)
+        # factors= lenstras_factorization(k, prime_num, points_list)
+        factors= divisor_using_multiplication(k,prime_num,points_list)
         stop = timeit.default_timer()
         (print(l_bound, '<= E(F_P)<=', r_bound))
         print(len(points_list))
